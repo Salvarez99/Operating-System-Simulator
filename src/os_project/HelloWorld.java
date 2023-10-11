01374682adceef03ef3eb8ca3b57e484e73181bb
Salvarez99 <Stephenalvarez3@gmail.com>
2023-10-11 19:56:52 -0400

    .

--- src/os_project/HelloWorld.java
+++ src/os_project/HelloWorld.java
@@ -8,12 +8,17 @@ public class HelloWorld extends UserlandProcess{
 	@Override
 	public void run() {
 		
-		try {
-			while(true) {
-				System.out.println("Hello World");
-				Thread.sleep(50); // sleep for 50 ms
-			}
-		} catch (Exception e) { }
+		OS os = new OS();
+		// try {
+		// 	while(true) {
+		// 		System.out.println("Hello World");
+		// 		Thread.sleep(50); // sleep for 50 ms
+
+		// 	}
+		// } catch (Exception e) { }
+		int id = os.Open("random 100");
+		os.Read(id, 10);
+		os.Close(id);
 		
 	}
 }
